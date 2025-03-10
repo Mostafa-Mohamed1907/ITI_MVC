528dafad3e7392d46b5b6f1b6311fdcdc09b82a4
Mostafa M.abdelnaser <mostafa.mohamed851@yahoo.com>
2025-03-11 01:59:09 +0200

    #CreatingNewDepartment

--- ITI_MVC/Controllers/DepartmentController.cs
+++ ITI_MVC/Controllers/DepartmentController.cs
@@ -5,11 +5,28 @@ namespace ITI_MVC.Controllers
 {
     public class DepartmentController : Controller
     {
+        ITIContext context = new ITIContext();
         public IActionResult Index()
         {
-            ITIContext context = new ITIContext();
-            List<Department> dept = context.Department.ToList();
+            
+            List<Department> dept = context.Department!.ToList();
             return View("Index", dept);
         }
+
+        public IActionResult Add()
+        {
+            return View("Add");
+        }
+        public IActionResult SaveDepartment(Department newDeptFromRequest)
+        {
+            if (newDeptFromRequest.Name != null)
+            {
+                context.Department!.Add(newDeptFromRequest);
+                context.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            return View("Add", newDeptFromRequest);
+        }
+
     }
 }
