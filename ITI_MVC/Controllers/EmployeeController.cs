ab6647788f15e75cb028668fc95099d36ed3d66c
Mostafa M.abdelnaser <mostafa.mohamed851@yahoo.com>
2025-03-13 16:04:46 +0200

    #EmpDeptListViewModel(to Edit on Employee Data)

--- ITI_MVC/Controllers/EmployeeController.cs
+++ ITI_MVC/Controllers/EmployeeController.cs
@@ -33,9 +33,9 @@ namespace ITI_MVC.Controllers
             branches.Add("Assuit");
             branches.Add("Menofia");
             branches.Add("Giza");
-            EmpDeptColorTempMsgBrnchViewModel EmpDeptVM = 
+            EmpDeptColorTempMsgBrnchViewModel EmpDeptVM =
                 new EmpDeptColorTempMsgBrnchViewModel();
-            
+
             Employee empModel = context.Employee.Include("Department").FirstOrDefault(p => p.Id == id);
             EmpDeptVM.EmpName = empModel.Name;
             EmpDeptVM.DeptName = empModel.Department.Name;
@@ -46,6 +46,56 @@ namespace ITI_MVC.Controllers
             EmpDeptVM.Color = "Red";
             return View("DetailsVM", EmpDeptVM);
         }
+        public IActionResult Index()
+        {
+            var employess = context.Employee.ToList();
+            return View("Index", employess);
+        }
+        //public IActionResult Edit(int id)
+        //{
+        //    Employee employeeModel = context.Employee.FirstOrDefault(E => E.Id == id);
+        //    return View("Edit", employeeModel);
+        //}
+
+        public IActionResult Edit(int id)
+        {
+            Employee employeeModel = context.Employee.FirstOrDefault(E => E.Id == id);
+            EmpDeptListViewModel EmpDeptListViewMOdel = new EmpDeptListViewModel();
+            List<Department> DepartmentList = context.Department.ToList();
+            EmpDeptListViewMOdel.Id = employeeModel.Id;
+            EmpDeptListViewMOdel.Name = employeeModel.Name;
+            EmpDeptListViewMOdel.Salary = employeeModel.Salary;
+            EmpDeptListViewMOdel.Address = employeeModel.Address;
+            EmpDeptListViewMOdel.ImageURL = employeeModel.ImageURL;
+            EmpDeptListViewMOdel.JobTitle = employeeModel.JobTitle;
+            EmpDeptListViewMOdel.DepartmentID = employeeModel.DepartmentID;
+            EmpDeptListViewMOdel.DeptList = DepartmentList;
+            return View("Edit", EmpDeptListViewMOdel);
+        }
+
+        public IActionResult SaveEdit(EmpDeptListViewModel EmpDeptFromREquest, int id)
+        {
+            Employee EmployeeFromDB = context.Employee.FirstOrDefault(e => e.Id == id);
+            if (EmpDeptFromREquest.Name != null)
+            {
+                //if (EmployeeFromDB == null)
+                //{
+                //    return NotFound("Employee not found");
+                //}
+                List<Department> DepartmentList = context.Department.ToList();
+                EmployeeFromDB.Name = EmpDeptFromREquest.Name;
+                EmployeeFromDB.Salary = EmpDeptFromREquest.Salary;
+                EmployeeFromDB.Address = EmpDeptFromREquest.Address;
+                EmployeeFromDB.ImageURL = EmpDeptFromREquest.ImageURL;
+                EmployeeFromDB.JobTitle = EmpDeptFromREquest.JobTitle;
+                EmployeeFromDB.DepartmentID = EmpDeptFromREquest.DepartmentID;
+                context.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            return View("Edit", EmpDeptFromREquest);
+
+        }
 
     }
 }
+
---
+++ ITI_MVC/ViewModel/EmpDeptListViewModel.cs
@@ -0,0 +1,17 @@
+﻿using ITI_MVC.Models;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace ITI_MVC.ViewModel
+{
+    public class EmpDeptListViewModel
+    {
+        public int Id { get; set; }
+        public string? Name { get; set; }
+        public int Salary { get; set; }
+        public string? JobTitle { get; set; }
+        public string? ImageURL { get; set; }
+        public string? Address { get; set; }
+        public int DepartmentID { get; set; }
+        public List<Department>? DeptList { get; set; }
+    }
+}
